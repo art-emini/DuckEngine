1603cee0383647ee44db21f6df7b5781455e5f43
ksplatdev <aviatorbuzz921@gmail.com>
2021-07-05 12:06:55 -0400

    added animateFrames method to spritesheet

--- src/core/gameobjects/spritesheet.ts
+++ src/core/gameobjects/spritesheet.ts
@@ -86,6 +86,39 @@ export default class SpriteSheet extends GameObject {
 		}, timeMS);
 	}
 
+	public animateFrames(
+		fps: number,
+		col: number,
+		row: number,
+		stopCol: number,
+		stopRow: number,
+		switchRowOnEnd?: boolean
+	) {
+		this.currentCol = col;
+		this.currentRow = row;
+
+		this.animationInterval = setInterval(() => {
+			if (this.animating) {
+				if (this.currentCol < stopCol - 1) {
+					this.nextCol();
+
+					if (this.currentCol === this.cols && !switchRowOnEnd) {
+						this.currentCol = 1; // draw subtracts one so it equals 0
+					}
+
+					if (this.currentCol === this.cols && switchRowOnEnd) {
+						if (this.currentRow < stopRow) {
+							this.currentCol = 1; // draw subtracts one so it equals 0
+							this.nextRow();
+						}
+					}
+				} else {
+					this.stopAnimation();
+				}
+			}
+		}, 1000 / fps);
+	}
+
 	public stopAnimation() {
 		this.animating = false;
 
