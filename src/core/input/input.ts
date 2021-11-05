240eb55fc3f24d8890a882cc7e7da82a6a07534f
ksplatdev <aviatorbuzz921@gmail.com>
2021-11-04 20:54:29 -0400

    Removed use of eventEmitter in input

--- src/core/input/input.ts
+++ src/core/input/input.ts
@@ -73,12 +73,7 @@ export default class Input {
 		description: string,
 		cb: (e: KeyboardEvent) => void
 	) {
-		document.addEventListener(type, (e) => {
-			this.game.eventEmitter.emit(
-				`INPUT_${description.toUpperCase()}`,
-				e
-			);
-		});
+		document.addEventListener(type, cb);
 		this.game.eventEmitter.on(`INPUT_${description.toUpperCase()}`, cb);
 		if (this.game.config.debug) {
 			new Debug.Log('Added event listener to Input.');
